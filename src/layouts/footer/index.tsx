1a0c2b3f13ffa10c94d51e320576e8e17daa0c7e
ThatOneStas <138216528+ThatOneStas@users.noreply.github.com>
2023-12-01 12:23:21 +0200

    01.12

--- src/layouts/footer/index.tsx
+++ src/layouts/footer/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import style from "./footer.module.scss";
+import s from "./footer.module.scss";
 // link
 import Link from 'next/link'
 // images
@@ -8,7 +8,47 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <>
-      <footer className={style.footer}>Footer</footer>
+      <footer className={s.footer}>
+        <div className={s.footer__main}>
+          <div className={s.footer__main_logo}>
+            <Link href={"/"}><h1>Morent</h1></Link>
+            <h2>
+              Our vision is to provide convenience and help increase your sales business.
+            </h2>
+          </div>
+          <div className={`${s.footer__main_box} ${s.about}`}>
+            <h1>About</h1>
+            <ul>
+              <li><Link href={"/"}>How it works</Link></li>
+              <li><Link href={"/"}>How it works</Link></li>
+              <li><Link href={"/"}>How it works</Link></li>
+              <li><Link href={"/"}>How it works</Link></li>
+            </ul>
+          </div>
+          <div className={`${s.footer__main_box} ${s.community}`}>
+            <h1>Community</h1>
+            <ul>
+              <li><Link href={"/"}>How it works</Link></li>
+              <li><Link href={"/"}>How it works</Link></li>
+              <li><Link href={"/"}>How it works</Link></li>
+              <li><Link href={"/"}>How it works</Link></li>
+            </ul>
+          </div>
+          <div className={`${s.footer__main_box} ${s.socials}`}>
+            <h1>Socials</h1>
+            <ul>
+              <li><Link href={"/"}>How it works</Link></li>
+              <li><Link href={"/"}>How it works</Link></li>
+              <li><Link href={"/"}>How it works</Link></li>
+              <li><Link href={"/"}>How it works</Link></li>
+            </ul>
+          </div>
+        </div>
+        <div className={s.footer__line}></div>
+        <div className={s.footer__sub}>
+
+        </div>
+      </footer>
     </>
   );
 };
--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -1,17 +1,52 @@
 import React from 'react'
-import style from "./header.module.scss"
+import s from "./header.module.scss"
 // link
 import Link from 'next/link'
 // images
 import Image from 'next/image'
+import Heart from "@/assets/img/heart.svg"
+import Bell from "@/assets/img/notification.svg"
+import Settings from "@/assets/img/setting-2.svg"
+import User from "@/assets/img/frame.svg"
+import Search from "@/assets/img/search-normal.svg"
+import Filter from "@/assets/img/filter.svg"
 
 
 const header = () => {
   return (
-    <header className={style.header}>
-      <Link href={'/'}>Home</Link>
-      <Link href={'/favourite'}>Fav</Link>
-      <Link href={'/cta'}>cta</Link>
+    <header className={s.header}>
+      <Link className={s.header__logo} href={"/"}>
+        <h1>Morent</h1>
+      </Link>
+      <div>
+        <input className={s.header__search} type="search" />
+        <Image className={s.header__searchIcon} src={Search} alt='Search-icon'></Image>
+        <Image className={s.header__filterIcon} src={Filter} alt='Filter-icon'></Image>
+      </div>
+      <nav className={s.header__nav}>
+        <ul>
+          <li>
+            <Link href={"/favourite"}>
+              <Image src={Heart} alt='heart-icon'></Image>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/"}>
+              <Image src={Bell} alt='Bell-icon'></Image>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/"}>
+              <Image src={Settings} alt='Settings-icon'></Image>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/"}>
+              <Image className={s.header__nav_userImg} src={User} alt='Profile-icon'></Image>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
